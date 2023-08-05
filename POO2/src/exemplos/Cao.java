aebbd32a911e7b5f04fc528405f61db34cffb1f3
Thiago Camillo <101370793+ThiCamillo@users.noreply.github.com>
2023-08-05 20:29:28 -0300

    Update Cao.java

--- POO2/src/exemplos/Cao.java
+++ POO2/src/exemplos/Cao.java
@@ -2,4 +2,19 @@ package exemplos;
 
 public class Cao {
 
+  String nome;
+  String raca;
+  double peso;
+
+  public static void falar(){
+    if(peso > 60) {
+      System.out.println("Woof, Woof, Woof");
+    }else{
+      if(peso < 15){
+      System.out.println("Yip, Yip, Yip");
+      }else{
+        System.out.println("Ruff, Ruff, Ruff");
+      }
+    }
+  }
 }
