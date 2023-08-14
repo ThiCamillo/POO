4f7df0e47fa3cf8c03b1c8e32536defbbe5116ed
thiago.camillo <thiago.camillo@FLE211D41.alunos.sc.senac.br>
2023-08-14 20:25:14 -0300

    constructor

---
+++ POO3/src/exemplos/ProgramaVeiculo.java
@@ -0,0 +1,12 @@
+package exemplos;
+
+public class ProgramaVeiculo {
+
+	public static void main(String[] args) {
+
+		Carro c1 = new Carro("Focus", "Ford", 2020, 45000.00, 170);
+		System.out.println("Modelo:" + c1.getModelo());
+		
+	}
+
+}
