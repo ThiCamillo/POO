5f664873ca1f5237657f0fb8f0d75d9096c2101b
thiago.camillo <thiago.camillo@FLE211D41.alunos.sc.senac.br>
2023-08-21 21:19:19 -0300

    nova classe

---
+++ POO3/src/lista_heranca/ExecutavelEletro.java
@@ -0,0 +1,11 @@
+package lista_heranca;
+
+public class ExecutavelEletro {
+
+	public static void main(String[] args) {
+		//Fogao
+				Fogao f1 = new Fogao("Mais Sabor", "Continental", 17, 5);
+				Fogao f2 = new Fogao("CookTop", "Muller", 5, 4);
+			}
+			
+		}
