2579bbf5556809544d44d28afce5aa2017d95c99
ThiCamillo <thiago.sam.camillo@gmail.com>
2023-08-28 20:08:39 -0300

    update

--- POO3/src/lista_heranca/Cozinha.java
+++ POO3/src/lista_heranca/Cozinha.java
@@ -16,12 +16,18 @@ public class Cozinha {
 		LavaLouca ll1 = new LavaLouca("LL14X", "Continental", 4, 45);
 		fornoEletrico fe1 = new fornoEletrico("forninho", "Mondial", 3, 15);
 
-		double consumoMensal = g1.calcularConsumo() + f1.calcularConsumo() + m1.calcularConsumo()
-				+ ll1.calcularConsumo() + fe1.calcularConsumo();
+		double consumoMensal = g1.calcularConsumo() 
+				+ f1.calcularConsumo() 
+				+ m1.calcularConsumo()
+				+ ll1.calcularConsumo() 
+				+ fe1.calcularConsumo();
 		System.out.println("Consumo Mensal da cozinha: " + consumoMensal);
 
-		double consumoPeriodo = g1.calcularConsumo(10) + f1.calcularConsumo(10) + m1.calcularConsumo(10)
-				+ ll1.calcularConsumo(10) + fe1.calcularConsumo(10);
+		double consumoPeriodo = g1.calcularConsumo(10)
+				+ f1.calcularConsumo(10)
+				+ m1.calcularConsumo(10)
+				+ ll1.calcularConsumo(10)
+				+ fe1.calcularConsumo(10);
 		System.out.println("Consumo no periodo de 10 dias da cozinha: " + consumoPeriodo);
 	}
 
