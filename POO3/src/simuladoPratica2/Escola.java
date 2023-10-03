59c44a83ef7b20a5bbbdbd0deebf21c1b2376a8e
Thiago Camillo <thiago.sam.camillo@gmail.com>
2023-10-03 01:06:33 -0300

    update class

--- POO3/src/simuladoPratica2/Escola.java
+++ POO3/src/simuladoPratica2/Escola.java
@@ -6,6 +6,7 @@ public class Escola {
 	private String telefone;
 	private Endereco enderecoEscola;
 	private Turma[] turmas;
+
 	public Escola(String nome, String telefone, Endereco enderecoEscola, Turma[] turmas) {
 		super();
 		this.nome = nome;
@@ -13,30 +14,85 @@ public class Escola {
 		this.enderecoEscola = enderecoEscola;
 		this.turmas = turmas;
 	}
+
 	public Escola() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public Endereco getEnderecoEscola() {
+		return enderecoEscola;
+	}
+
+	public void setEnderecoEscola(Endereco enderecoEscola) {
+		this.enderecoEscola = enderecoEscola;
+	}
+
+	public Turma[] getTurmas() {
+		return turmas;
+	}
+
+	public void setTurmas(Turma[] turmas) {
+		this.turmas = turmas;
+	}
+
 	public void contabilizarAlunas() {
-		// TODO Auto-generated method stub
+		int total = 0;
+		for (Turma turma : this.getTurmas()) {
+			total += turma.getAlunos().length;
+		}
+		System.out.println("O total de alunas é: " + total);
+	}
+
+	public void localizarEnderecoRegente(String nome) {
+		System.out.println("\nEndereço do regente: " + nome);
+		for (Turma turma : this.getTurmas()) {
+			if (turma.getRegente().getNome().equals(nome)) {
+				System.out.println(turma.getRegente().getEndereco());
+			}
+		}
+	}
+
+	public void listarAlunasPorSerie(int serie) {
+		System.out.println("\nLista de Alunas da Série: " + serie);
+		for(int i=0; i< this.getTurmas().length; i++) {
+			if(this.getTurmas()[i].getSerie() == serie) {
+				this.getTurmas()[i].listarAlunas();
+			}
+		}
 		
 	}
-	public void localizarEnderecoRegente(String string) {
-		// TODO Auto-generated method stub
-		
-	}
-	public void listarAlunasPorSerie(int i) {
-		// TODO Auto-generated method stub
-		
-	}
+
 	public void identificarRegenteMaisJovem() {
-		// TODO Auto-generated method stub
-		
+		Professor maisNovo = this.getTurmas()[0].getRegente();
+		for(int i=0; i< this.getTurmas().length; i++) {
+			if(this.getTurmas()[i].getRegente().getIdade() < maisNovo.getIdade()) {
+				maisNovo = this.getTurmas()[i].getRegente();
+			}
+		}
+		System.out.println("\nRegente mais Novo: " + maisNovo.getNome());
 	}
-	public void listarAlunasPorCidade(String string) {
-		// TODO Auto-generated method stub
-		
+
+	public void listarAlunasPorCidade(String cidade) {
+		System.out.println("\nLista de Alunas da cidade: " + cidade);
+		for(int i=0; i<this.getTurmas().length; i++) {
+			this.getTurmas()[i].listarAlunasPorCidade(cidade);
+		}
 	}
-	
-	
+
 }
--- POO3/src/simuladoPratica2/Turma.java
+++ POO3/src/simuladoPratica2/Turma.java
@@ -5,35 +5,53 @@ public class Turma {
 	private int serie;
 	private Professor regente;
 	private Aluno[] alunos;
+
 	public Turma(int serie, Professor regente, Aluno[] alunos) {
 		super();
 		this.serie = serie;
 		this.regente = regente;
 		this.alunos = alunos;
 	}
+
 	public Turma() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
+
 	public int getSerie() {
 		return serie;
 	}
+
 	public void setSerie(int serie) {
 		this.serie = serie;
 	}
+
 	public Professor getRegente() {
 		return regente;
 	}
+
 	public void setRegente(Professor regente) {
 		this.regente = regente;
 	}
+
 	public Aluno[] getAlunos() {
 		return alunos;
 	}
+
 	public void setAlunos(Aluno[] alunos) {
 		this.alunos = alunos;
 	}
 
-	
-	
+	public void listarAlunas() {
+		for (Aluno aluna : this.getAlunos()) {
+			System.out.println(aluna.getNome());
+		}
+	}
+
+	public void listarAlunasPorCidade(String cidade) {
+		for (int i = 0; i < this.getAlunos().length; i++) {
+			if (this.getAlunos()[i].getEndereco().getCidade().equals(cidade)) {
+				System.out.println(this.getAlunos()[i].getNome());
+			}
+		}
+	}
 }
