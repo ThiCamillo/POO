15ee5b00c6562b62368c02080c5ca11b0f368adf
ThiCamillo <thiago.sam.camillo@gmail.com>
2023-10-09 21:04:04 -0300

    new simulado of datas

---
+++ POO3/src/simuladoPratica3/Endereco.java
@@ -0,0 +1,5 @@
+package simuladoPratica3;
+
+public class Endereco {
+
+}
---
+++ POO3/src/simuladoPratica3/Executavel.java
@@ -0,0 +1,10 @@
+package simuladoPratica3;
+
+public class Executavel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
